132779496bd4d559841598d9df795d8051c54702
DannyRyd <danielrydenberg@gmail.com>
2024-05-15 11:42:17 +0200

    initial commit

---
+++ src/main/java/com/practice/worthreadingservice24/Worthreadingservice24Application.java
@@ -0,0 +1,13 @@
+package com.practice.worthreadingservice24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Worthreadingservice24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Worthreadingservice24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/worthreadingservice24/Worthreadingservice24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.worthreadingservice24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Worthreadingservice24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
